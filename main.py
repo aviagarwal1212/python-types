3a1f22bde643df914238119d159b25dc59973997
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-07 12:44:04 +0000

    complete dictionaries

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-my_set: set = {1, 2, 3}
-letters: set[str] = {"a", "b", "c"}
-mixed: set[str | int] = {"a", 2, "c"}
+my_dict: dict = {"a": 10}
+typed_dict: dict[str, int] = {"a": 10}
+test_dict: dict[int, str] = {1: "first", 2: "second"}
+
+
+def print_it(some_dict: dict[int, str]):
+    for value in some_dict.values():
+        print(value.title())
+
+
+print_it(test_dict)
+
+# error
+# print_it(typed_dict)
