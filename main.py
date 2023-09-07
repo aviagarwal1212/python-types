95c69bd845ffd8e7037865ae2d9ed2ec94432caf
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-07 12:11:10 +0000

    complete sets

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-coordinate: tuple[int, int] = (20, 30)
-coordinate_2: tuple[int, int, int, int] = (10, 20, 30, 40)
-coordinate_3: tuple[int, ...] = (10, 20, 30, 40)
-coordinate_4: tuple[int | str, ...] = (10, 20, "30", "40")
-
-
-def test():
-    print()
+my_set: set = {1, 2, 3}
+letters: set[str] = {"a", "b", "c"}
+mixed: set[str | int] = {"a", 2, "c"}
