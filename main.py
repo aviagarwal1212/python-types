305ed0ab9f1a3b91c1a9b35268bf9371a5f30fd8
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-07 10:52:17 +0000

    complete basic types

---
+++ main.py
@@ -0,0 +1,10 @@
+var: str = 'hello'
+number: int = 10
+percent: float = 0.50
+connected: bool = True
+
+def format_input(user_input: str) -> None:
+    print(user_input.capitalize())
+
+format_input('hello world!')
+
