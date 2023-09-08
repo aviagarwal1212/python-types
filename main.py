f40bb4add5a5ed630f0957a8c009cf8785bfdf63
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-08 07:13:35 +0000

    complete external types

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-def add_numbers(a: int, b: int) -> int:
-    return a + b
+import requests
+from requests import Response
 
 
-# error
-# add_numbers(10, 20.0)
+def get_status(url: str) -> int:
+    request: Response = requests.get(url)  # how do we annotate this?
+    status_code: int = request.status_code
+    return status_code
 
 
-def return_nothing(a: int) -> None:
-    # error
-    # return a
-    print(a)
-
-
-def return_nothing_also(a: int):
-    # no error
-    return a
-
-
-def fetch_user(user_id: int) -> str | None:  # returning an optional
-    users: dict[int, str] = {0: "Mario", 1: "Luigi"}
-    return users.get(user_id)
-
-
-fetch_user(1)
-fetch_user(99)
+print(get_status("https://www.google.com"))
