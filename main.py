adc5e507def3ac6c34f7d1a6aa4632eebf88d976
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-18 09:13:39 +0000

    complete protocol

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-from typing import Callable
-from datetime import datetime
+from typing import Protocol
+from dataclasses import dataclass
 
 
-def get_time() -> str:
-    return f"{datetime.now():%H:%M:%S}"
+class Printer(Protocol):
+    def print(self, magazine: str) -> None:
+        ...
+
+    def copy(self, magazine: str, copies: int) -> None:
+        ...
 
 
-def repeat(func: Callable, amount: int) -> None:
-    for i in range(amount):
-        print(i + 1)
-        print(func())
+@dataclass
+class LazerPrinter:
+    name: str
+    version: int
+
+    def print(self, magazine: str) -> None:
+        print(f"{self.name} (v{self.version}) is printing: '{magazine}'")
+
+    def copy(self, magazine: str, copies: int) -> None:
+        print(
+            f"{self.name} (v{self.version}) is printing {copies} copies of: '{magazine}'"
+        )
 
 
-repeat(get_time, 3)
-# repeat("bla", 5) # error
+@dataclass
+class InkJetPrinter:
+    name: str
+    version: int
+
+    def print(self, magazine: str) -> None:
+        print(f"{self.name} (v{self.version}) is printing: '{magazine}'")
+
+    def copy(self, magazine: str, copies: int) -> None:
+        print(
+            f"{self.name} (v{self.version}) is printing {copies} copies of: '{magazine}'"
+        )
 
 
-def print_it(text: str, printer: Callable[[str], None]) -> None:
-    printer(text)
+def print_magazine(printer: Printer, magazine: str) -> None:
+    printer.print(magazine)
+    printer.copy(magazine, 5)
 
 
-def loud_print(text: str) -> None:
-    print(text.upper())
+lp: LazerPrinter = LazerPrinter("Lazer Printer", 1)
+
+lp_printer: Printer = LazerPrinter(
+    "Lazer Printer", 2
+)  # no errors because Lazerprinter implements print and copy
+
+ijp: InkJetPrinter = InkJetPrinter("InkJet Printer", 1)
 
 
-def print_number(number: int) -> None:
-    print(number)
-
-
-def louder(text: str) -> str:
-    return text.upper()
-
-
-print_it("hello", loud_print)
-print_it("hello", print_number)  # error
-print_it("hello", louder)  # error
+print_magazine(lp, "Go")
+print_magazine(ijp, "Python")
