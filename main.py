71065d6469e2d44f482a8f721798959cfa472f89
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-18 11:36:40 +0000

    complete typeddict

--- main.py
+++ main.py
@@ -1,56 +1,15 @@
-from typing import Protocol
-from dataclasses import dataclass
+from typing import TypedDict, Required, NotRequired
 
 
-class Printer(Protocol):
-    def print(self, magazine: str) -> None:
-        ...
-
-    def copy(self, magazine: str, copies: int) -> None:
-        ...
-
-
-@dataclass
-class LazerPrinter:
-    name: str
-    version: int
-
-    def print(self, magazine: str) -> None:
-        print(f"{self.name} (v{self.version}) is printing: '{magazine}'")
-
-    def copy(self, magazine: str, copies: int) -> None:
-        print(
-            f"{self.name} (v{self.version}) is printing {copies} copies of: '{magazine}'"
-        )
+class Coordinate(TypedDict):
+    x: float
+    y: float
+    label: str
+    category: NotRequired[str]
 
 
-@dataclass
-class InkJetPrinter:
-    name: str
-    version: int
-
-    def print(self, magazine: str) -> None:
-        print(f"{self.name} (v{self.version}) is printing: '{magazine}'")
-
-    def copy(self, magazine: str, copies: int) -> None:
-        print(
-            f"{self.name} (v{self.version}) is printing {copies} copies of: '{magazine}'"
-        )
+coordinate: Coordinate = {"x": 10, "y": 20, "label": "Profit"}
 
 
-def print_magazine(printer: Printer, magazine: str) -> None:
-    printer.print(magazine)
-    printer.copy(magazine, 5)
-
-
-lp: LazerPrinter = LazerPrinter("Lazer Printer", 1)
-
-lp_printer: Printer = LazerPrinter(
-    "Lazer Printer", 2
-)  # no errors because Lazerprinter implements print and copy
-
-ijp: InkJetPrinter = InkJetPrinter("InkJet Printer", 1)
-
-
-print_magazine(lp, "Go")
-print_magazine(ijp, "Python")
+Vote = TypedDict("Vote", {"for": int, "against": int})
+vote: Vote = {"for": 100, "against": 200}
