da041bdba0ba7ff468502307c8ac5006927d81ff
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-18 12:01:49 +0000

    complete newtype

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from dataclasses import dataclass
-from typing import TypeAlias, Callable, Literal
-
-OptionalStr: TypeAlias = str | None
-Mode: TypeAlias = Literal["r"]
-
-Printer: TypeAlias = Callable[[str], None]
-
-FruitType: TypeAlias = "Fruit"
+from typing import NewType, TypeAlias
 
 
-@dataclass
-class Fruit:
-    name: str
+UserId: TypeAlias = int
 
-    def fruit_method(self) -> FruitType:
-        return self
+
+def get_user(userid: UserId) -> str | None:
+    users: dict = {0: "Mario", 1: "Luigi", 2: "Peach"}
+    return users.get(userid)
+
+
+print(get_user(0))
+print(get_user(False))
+print(get_user(UserId(0)))
+
+
+NewUserId = NewType("NewUserId", int)
+
+
+def get_another_user(userid: NewUserId) -> str | None:
+    ...
+
+
+print(get_another_user(0))  # error
+print(get_another_user(False))  # error
+print(get_another_user(NewUserId(0)))
